c2511fb85f552b0794fd7001fd48618147367d36
Joshua Echeverri <josheche@gmail.com>
2020-03-28 13:37:37 -0400

    SVG icon updates. Refactored site-nav.

--- assets/bundle.js
+++ assets/bundle.js
@@ -8208,7 +8208,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54177" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49393" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
